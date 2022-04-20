e1fd8c3513f90e253ef5453023335891bc5c6b64
timkwali <timkwalit@accessbankplc.com>
2022-04-20 17:12:12 +0100

    add ci/cd

--- app/src/main/java/com/adyen/android/assignment/common/utils/Constants.kt
+++ app/src/main/java/com/adyen/android/assignment/common/utils/Constants.kt
@@ -1,7 +1,6 @@
 package com.adyen.android.assignment.common.utils
 
 object Constants {
-
     const val PLACES_DATABASE_NAME = "places_database"
     const val PLACES_TABLE_NAME = "places_table"
 }
